a5f9358bc443d684397aa76db9cd948bebd6bbeb
mhhung <mhhung@cht.com.tw>
2025-09-22 16:39:01 +0800

    feat: 根據環境變數調整輸出設定與資產前綴，改善生產環境配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: isProd ? 'export' : undefined,
+  reactStrictMode: true,
+  assetPrefix: isProd
+    ? 'https://tinda.github.io/toolguy.github.io/'
+    : undefined
   /* config options here */
 };
 
